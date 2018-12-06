d1d78b70e8c21b1df2cc2c79c7976e5d50de8e7d
SmallW <447587073@qq.com>
2018-12-06 13:58:08 +0800

    change

--- build/server.js
+++ build/server.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const webpack = require("webpack");
+const webpackMiddleware = require("webpack-dev-middleware");
+const webpackHotMiddleware = require("webpack-hot-middleware");
 
 const app = express();
 
